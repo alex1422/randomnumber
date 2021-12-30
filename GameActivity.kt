a271f40ac33a76ad99f2fa2fef511909fdd9adb1
alex1422 <90585258+alex1422@users.noreply.github.com>
2021-12-30 18:41:44 +0800

    Add files via upload

---
+++ GameActivity.kt
@@ -0,0 +1,47 @@
+package com.example.randomfilmk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Button
+
+class GameActivity : AppCompatActivity() {
+    var min:Int = 0
+    var max:Int = 0
+    var m:Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+        min = intent.getIntExtra("min", 0)
+        max = intent.getIntExtra("max", 100)
+        Solving()
+    }
+
+    fun Solving(){
+        var l = max - min + 1
+        m = (l/2).toInt() + min - 1
+
+        findViewById<TextView>(R.id.text_1).text = "Это число больше " + m.toString() + "?"
+        if (l == 1){
+            finishFunc()
+        }
+    }
+    fun finishFunc() {
+        findViewById<TextView>(R.id.text_1).text = "Это  " + min.toString()
+        findViewById<Button>(R.id.text_2).isEnabled = false
+        findViewById<Button>(R.id.text_3).isEnabled = false
+    }
+
+    fun onYesNoClick(view: android.view.View) {
+        when (view.id) {
+            R.id.text_2 -> {
+                min = m + 1
+            }
+            R.id.text_3 -> {
+                max = m
+            }
+        }
+        Solving()
+    }
+}
